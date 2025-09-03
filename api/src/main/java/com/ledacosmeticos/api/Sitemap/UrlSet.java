6108710c60c64e058ed7e3e8d5a279521717ae41
Jan Luiz <103154512+devjanBarbosa@users.noreply.github.com>
2025-09-03 15:22:15 -0300

    fix

--- api/src/main/java/com/ledacosmeticos/api/Sitemap/UrlSet.java
+++ api/src/main/java/com/ledacosmeticos/api/Sitemap/UrlSet.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @JacksonXmlRootElement(localName = "urlset", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
 public class UrlSet {
+
     @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "url", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
-    private List<Url> url = new ArrayList<>();
+    @JacksonXmlProperty(localName = "url")
+    private List<Url> urls = new ArrayList<>();
 }
